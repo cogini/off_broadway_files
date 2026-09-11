3775831e19b48008ca13b4fdfc22d09fa88ad324
Jake Morrison <jake@cogini.com>
2026-09-11 16:41:19 -0500

    Generate project

---
+++ lib/off_broadway_files.ex
@@ -0,0 +1,18 @@
+defmodule OffBroadwayFiles do
+  @moduledoc """
+  Documentation for `OffBroadwayFiles`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OffBroadwayFiles.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/off_broadway_files_test.exs
@@ -0,0 +1,8 @@
+defmodule OffBroadwayFilesTest do
+  use ExUnit.Case
+  doctest OffBroadwayFiles
+
+  test "greets the world" do
+    assert OffBroadwayFiles.hello() == :world
+  end
+end
