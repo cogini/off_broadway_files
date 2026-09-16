b42b4b87e7eaa869ee14806a31f0121bbdcfd4ca
Jake Morrison <jake@cogini.com>
2026-09-16 14:53:06 -0500

    Doc

--- lib/off_broadway_files/pipeline.ex
+++ lib/off_broadway_files/pipeline.ex
@@ -14,6 +14,7 @@ defmodule OffBroadwayFiles.Pipeline do
     in_dir = args[:in_dir] || "/tmp/off_broadway_files"
     archive_dir = args[:archive_dir] || Path.join(in_dir, "archive")
     failed_dir = args[:failed_dir] || Path.join(in_dir, "failed")
+    # processors_concurrency = args[:processors_concurrency] || default: System.schedulers_online()
 
     producer_config = [
       in_dir: in_dir,
@@ -31,6 +32,7 @@ defmodule OffBroadwayFiles.Pipeline do
         module: {Producer, producer_config}
       ],
       processors: [
+        # default: System.schedulers_online()
         default: [concurrency: 1]
       ]
     )
